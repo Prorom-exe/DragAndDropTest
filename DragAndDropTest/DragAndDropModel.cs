d7ff9d09d3abf4c088c7bc8d2974392fc528299b
Бутяев Роман <ButyaevRI@kamaz.ru>
2022-08-08 00:29:03 +0300

    Добавьте файлы проекта.

---
+++ DragAndDropTest/DragAndDropModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DragAndDropTest
+{
+    public class DragAndDropModel
+    {
+        public int Id { get; set; }
+        public string  Label { get; set; }
+        public int Number { get; set; }
+    }
+}
